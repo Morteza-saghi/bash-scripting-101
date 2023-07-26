ab4b011ae9af6637a9bbd189398fb08316fc8901
Mo <mort.saghi@gmail.com>
2023-07-26 13:58:23 +0330

    101-1

---
+++ fileexist.sh
@@ -0,0 +1,13 @@
+
+echo "whats the file name " 
+read filename
+if [ ! -f "$filename" ]
+then echo "ok "
+else echo "error"
+fi
+
+
+
+# ! is like logaical not
+# what this file does  is that if the file
+# is not there it say its okay
