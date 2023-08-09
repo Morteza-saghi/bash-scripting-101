a0e3b6e67a0072e8087bf8d51ad249ae8661c0eb
Mo <mort.saghi@gmail.com>
2023-08-09 20:53:34 +0330

    comp operatorsin bash scripts

---
+++ ComparisonOPT.sh
@@ -0,0 +1,17 @@
+clear
+read -p "how old are you " age
+if [ "$age" -lt 24 ]
+then
+	echo "you are young sir! "
+else
+	echo "you better watch out!! old days are you little fuck "
+fi
+
+
+
+# -gt graeater than
+# -ge greater or equal
+# -lt less than
+# -le less or equal
+# -eq is equal to
+# -ne not equal
