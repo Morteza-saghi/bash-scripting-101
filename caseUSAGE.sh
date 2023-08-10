b90fb2457332d498706087287146c778d3e3e625
Mo <mort.saghi@gmail.com>
2023-08-10 15:39:43 +0330

    using case in bash 101

---
+++ caseUSAGE.sh
@@ -0,0 +1,14 @@
+read -p "please enter a name " name
+case "$name" in 
+
+"mo")
+echo "what ever you say sir ";;
+
+"javad")
+echo "you cant say shit in here bitch "
+;;
+
+*)
+echo "fuck off!!!!! "
+
+esac
