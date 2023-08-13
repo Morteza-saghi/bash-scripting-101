fcc7437e6bcb5f8129e3ddc601789f814f342fb4
Mo <mort.saghi@gmail.com>
2023-08-13 13:13:38 +0330

    an old condition forgot to push

---
+++ conditions-101.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+echo 'you are here '$PWD''
+if [ -d 'newDir' ]
+then echo 'you good to go'
+else echo 'dont have the needed directory ,wanna me to make it (y,n) ?'
+read userC
+if [ $userC == 'y' ]
+then mkdir newDir
+ls
+else echo 'bye'
+fi
+fi
