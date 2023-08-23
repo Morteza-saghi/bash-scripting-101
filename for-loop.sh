05cb163081f8f25f7a4c65b82c5c12b116a3dd05
Morteza <101218380+Morteza-saghi@users.noreply.github.com>
2023-08-23 13:37:40 +0330

    using for loop and some help for another kind

---
+++ for-loop.sh
@@ -0,0 +1,12 @@
+
+clear
+for i in {1..100}
+do
+echo $i
+done
+
+
+
+# in the in the format is just like this  {from..to..step}
+# so for with 5 distance count to 100  we can have this (0..100..5)
+#
