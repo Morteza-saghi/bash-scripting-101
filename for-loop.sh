e2fc84c94befa38ecd994604a69dab3cf602e73e
Morteza <101218380+Morteza-saghi@users.noreply.github.com>
2023-09-08 16:18:45 +0330

    Update for-loop.sh

--- for-loop.sh
+++ for-loop.sh
@@ -10,3 +10,33 @@ done
 # in the in the format is just like this  {from..to..step}
 # so for with 5 distance count to 100  we can have this (0..100..5)
 #
+
+
+
+
+fruits=("apple" "banana" "orange" "grape")  
+
+for fruit in "${fruits[@]}"
+do
+    echo "I like $fruit"
+done
+
+
+# ${fruits[@]} in Bash represents the entire array fruits
+# notation [@] allows you to access all elements within the array
+
+
+# In Bash, ${fruits[@]} and ${fruits[*]} are used to access all elements within an array. However, there is a key difference between the two.
+
+
+# Using ${fruits[@]}:
+# apple
+# banana
+# split
+# cherry
+__________________________________________________________________
+
+# Using ${fruits[*]}:
+# apple banana split cherry
+
+
