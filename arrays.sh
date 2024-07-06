8f48736bc82fc048d35b00630584addb26534e62
Morteza <101218380+Morteza-saghi@users.noreply.github.com>
2024-07-06 13:34:33 +0330

    arrays in bash

---
+++ arrays.sh
@@ -0,0 +1,13 @@
+arrays 
+
+
+to define an array we do like this 
+
+thisarr=(1) # an array with one value 
+
+thisarr=(1 2 3 4) # an array with 4 values
+
+
+how to add a value to the array
+
+thisarr+=(newchar)  
