bfcdad96a4baa38ca0bc492a46fe72c7a54ac58e
morteza-saghi <mort.saghi@gmail.com>
2024-08-20 20:10:09 +0330

    some changes

--- Odd_Even.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "give me num " num1
-
-if [ $((num1 % 2 )) -gt 0  ]
-then echo "this num is odd"
-
-else echo "this num is even"
-fi
--- and-opt-2condition.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-read -p "plz enter a number: " num1
-if [ $num1 -gt 10 ] && [ $num1  -lt 25  ]
-then echo "its okay"
-else echo "nope try again"
-fi
