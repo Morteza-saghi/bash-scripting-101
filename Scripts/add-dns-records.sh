2eb2a344b81bc93fea2b99fd28a00a55afc05798
Morteza <101218380+Morteza-saghi@users.noreply.github.com>
2024-09-06 17:25:28 +0330

    Update add-dns-records.sh

--- Scripts/add-dns-records.sh
+++ Scripts/add-dns-records.sh
@@ -3,24 +3,24 @@ echo "#403" > /etc/resolv.conf
 echo "#nameserver 10.202.10.202"  >> /etc/resolv.conf
 echo "#nameserver 10.202.10.102"   >> /etc/resolv.conf
 
-echo ======================================================================  >> /etc/resolv.>
+echo ======================================================================  >> /etc/resolv.conf
 
 echo "#shecan" >> /etc/resolv.conf
 echo "#nameserver 178.22.122.100"   >> /etc/resolv.conf
 echo "#nameserver 185.51.200.2"   >> /etc/resolv.conf
 
-echo ======================================================================  >> /etc/resolv.>
+echo ======================================================================  >> /etc/resolv.conf
 echo "#radar"   >> /etc/resolv.conf
 echo "#nameserver 10.202.10.10"   >> /etc/resolv.conf
 echo "#nameserver 10.202.10.11"   >> /etc/resolv.conf
 
-echo ======================================================================  >> /etc/resolv.>
+echo ======================================================================  >> /etc/resolv.conf
 
 echo "#begzar" >> /etc/resolv.conf
 echo "#nameserver 185.55.226.26 "  >> /etc/resolv.conf
 echo "#nameserver 185.55.225.25  "  >> /etc/resolv.conf
 
-echo ======================================================================  >> /etc/resolv.>
+echo ======================================================================  >> /etc/resolv.conf
 
 
 echo "#electro" >> /etc/resolv.conf
@@ -29,7 +29,8 @@ echo "#nameserver 78.157.42.101  "  >> /etc/resolv.conf
 
 
 
-echo ======================================================================  >> /etc/resolv.>
+echo ======================================================================  >> /etc/resolv.conf
 
 echo "nameserver 8.8.8.8"   >> /etc/resolv.conf
+
 
